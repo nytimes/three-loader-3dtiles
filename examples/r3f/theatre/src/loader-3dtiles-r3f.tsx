9805cd112811f49d52f2ab0449980f506d093225
Avnerus <avnerus@gmail.com>
2021-10-12 19:00:23 +0300

    Added also theatre example

---
+++ examples/r3f/theatre/src/loader-3dtiles-r3f.tsx
@@ -0,0 +1,60 @@
+import { Loader3DTiles, LoaderProps, Runtime } from 'three-loader-3dtiles'
+import { useLoader, useThree, useFrame } from '@react-three/fiber'
+import { Loader, Object3D } from 'three'
+import {editable as e, SheetProvider, extension} from '@theatre/r3f'
+
+class Loader3DTilesBridge extends Loader {
+  props: LoaderProps;
+
+  load(url, onLoad, onProgress, onError) {
+    const loadTileset = async () => {
+      try {
+        const result = await Loader3DTiles.load({
+           url,
+           ...this.props,
+          onProgress
+         })
+         onLoad(result);
+      }
+      catch(e) {
+        console.log("Error loading 3d tiles!", e);
+        onError(e);
+      }
+    }
+    loadTileset();
+  }
+  setProps(props) {
+    this.props = props;
+  }
+};
+
+function Loader3DTilesR3FAsset(props) {
+  const threeState = useThree();
+  const loaderProps = {
+    renderer: threeState.gl,
+    options: {
+      dracoDecoderPath: 'https://unpkg.com/three@0.129.0/examples/js/libs/draco',
+      basisTranscoderPath: 'https://unpkg.com/three@0.129.0/examples/js/libs/basis',
+      ...props
+    }
+  }
+
+  // TODO: Getting type error
+  // @ts-ignore
+  const { model, runtime } = useLoader(Loader3DTilesBridge, props.url, (loader:Loader3DTilesBridge) => {
+    loader.setProps(loaderProps);    
+  })
+
+  useFrame(({ gl, camera }, dt) => {
+    runtime.update(dt, gl, camera);
+  });
+
+  // TODO: Disposal throws an error from updateCacheStats
+  return (
+    <e.group {...props} dispose={runtime.dispose} uniqueName="tileset">
+      <primitive object={model} />
+    </e.group>
+  )
+}
+
+export { Loader3DTilesR3FAsset }

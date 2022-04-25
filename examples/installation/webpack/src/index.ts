de1015b701c55265849674738ef10e8484d55d13
Avnerus <avnerus@gmail.com>
2022-04-25 16:39:07 +0300

    update webpack demo

--- examples/installation/webpack/src/index.ts
+++ examples/installation/webpack/src/index.ts
@@ -5,8 +5,6 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
 import { KTX2Loader } from 'three/examples/jsm/loaders/KTX2Loader';
 import StatsWidget from '@probe.gl/stats-widget';
 
-import { OpenStreetMapsProvider, MapView, UnitsUtils, BingMapsProvider } from 'geo-three'
-
 import {
   Scene,
   PerspectiveCamera,
@@ -19,8 +17,8 @@ import {
 
 const scene = new Scene()
 
-const camera = new PerspectiveCamera( 45,1, 0.1, 10000000 );
-camera.position.set(0,0, 100);
+const camera = new PerspectiveCamera();
+camera.position.set(0,0,100);
 
 const renderer = new WebGLRenderer()
 renderer.outputEncoding = sRGBEncoding;
@@ -36,43 +34,30 @@ canvasParent.appendChild(renderer.domElement);
 let tilesRuntime = undefined;
 let statsRuntime = undefined;
 
-const gridHelper = new GridHelper(100,5);
-scene.add( gridHelper );
-
-const light = new AmbientLight( 0x404040 ); // soft white light
-scene.add( light );
-
-const provider = new BingMapsProvider();
-const map = new MapView(MapView.PLANAR, provider);
-scene.add(map)
 
 async function loadTileset() {
   const result = await Loader3DTiles.load( 
   {
-      url: 'https://storage.googleapis.com/rd-big-files/DRONE/2022/RC-TILESET/P1/0322_Presidents_P1_10M_78x8K__10K-PN_DB/tileset_tileset.json',
+      url: 'https://int.nyt.com/data/3dscenes/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json',
       renderer: renderer,
       options: {
         dracoDecoderPath: 'https://unpkg.com/three@0.137.0/examples/js/libs/draco',
         basisTranscoderPath: 'https://unpkg.com/three@0.137.0/examples/js/libs/basis',
-        debug: false,
-        pointCloudColoring: PointCloudColoring.RGB,
-        resetGeoTransform: false
+        debug: true,
+        pointCloudColoring: PointCloudColoring.RGB
       }
   }
   )
-  const {model, runtime} = result
+  const {model, runtime} = result;
+  scene.add(model);
+  scene.add(runtime.getTileBoxes());
+
   tilesRuntime = runtime
 
   statsRuntime = new StatsWidget(runtime.getStats(), {container: statsParent });
   statsParent.style.visibility = 'visible';
 
-  scene.add(runtime.getTileBoxes());
-  scene.add(model)
-
-  camera.position.copy(model.position);
-  camera.translateY(100);
-  controls.target.copy(model.position);
-  controls.update();
+  model.rotation.set(-Math.PI/2, 0, 0);
 }
 
 function render() {

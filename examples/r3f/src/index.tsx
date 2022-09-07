83f6a2eca91a36f1a7bf43e89ca51e7990174d5c
Avnerus <avnerus@gmail.com>
2022-09-07 14:33:54 +0300

    Bump versions in R3F example. Fixes issue #42

--- examples/r3f/src/index.tsx
+++ examples/r3f/src/index.tsx
@@ -1,8 +1,8 @@
-import ReactDOM from 'react-dom'
+import * as ReactDOM from 'react-dom/client'
 import { useState, useRef, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { PerspectiveCamera, OrbitControls } from '@react-three/drei'
-import { ErrorBoundary } from 'react-hooks-fetch';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Matrix4, Euler } from 'three'
 
 import { Loader3DTilesR3FAsset } from './loader-3dtiles-r3f'
@@ -14,12 +14,12 @@ function App() {
     <div id="canvas-container">
       <Canvas style={{ background: '#272730'}}>
         <PerspectiveCamera ref={camera}>
-          <ErrorBoundary fallback={
+          <ErrorBoundary fallbackRender={() => (
             <mesh>
               <sphereBufferGeometry />
               <meshBasicMaterial color="red" />
             </mesh>
-          }>
+          )}>
             <Suspense fallback={
               <mesh>
                 <sphereBufferGeometry />
@@ -27,8 +27,8 @@ function App() {
               </mesh>
             }>
               <Loader3DTilesR3FAsset
-                 dracoDecoderPath={"https://unpkg.com/three@0.137.0/examples/js/libs/draco"}
-                 basisTranscoderPath={"https://unpkg.com/three@0.137.0/examples/js/libs/basis"}
+                 dracoDecoderPath={"https://unpkg.com/three@0.138.0/examples/js/libs/draco"}
+                 basisTranscoderPath={"https://unpkg.com/three@0.138.0/examples/js/libs/basis"}
                  rotation={new Euler(-Math.PI / 2, 0, 0)}
                  url="https://int.nyt.com/data/3dscenes/ONA360/TILESET/0731_FREEMAN_ALLEY_10M_A_36x8K__10K-PN_50P_DB/tileset_tileset.json"
                 maximumScreenSpaceError={48}
@@ -42,4 +42,5 @@ function App() {
   )
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);

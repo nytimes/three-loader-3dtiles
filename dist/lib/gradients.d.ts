6fac101a2c9c60ebb232797f979f065cbde84d15
avnerus <avnerus@gmail.com>
2024-02-20 09:48:26 +0000

    Squashed commit of the following:
    commit e9551856d2b7d7b8c0eb5850eb3f96347e205a01
    Author: avnerus <avnerus@gmail.com>
    Date:   Tue Feb 20 09:41:43 2024 +0000
    
        1.2.1
    
    commit c00423bf22673bc24164740de7153a1ab928a644
    Author: avnerus <avnerus@gmail.com>
    Date:   Tue Feb 20 09:41:21 2024 +0000
    
        restore gh-pages ci
    
    commit d0df45349257232ce019afe85a8accacb6f8b50f
    Author: avnerus <avnerus@gmail.com>
    Date:   Tue Feb 20 09:31:37 2024 +0000
    
        test gh-pages
    
    commit b053d498217917da68e8de0e32d7ad233ac8a12b
    Author: avnerus <avnerus@gmail.com>
    Date:   Tue Feb 20 09:25:57 2024 +0000
    
        fix readme build tag and drone gh-pages script

---
+++ dist/lib/gradients.d.ts
@@ -0,0 +1,7 @@
+import { Color } from 'three';
+declare type Gradient = Array<[number, Color]>;
+declare const Gradients: {
+    [key: string]: Gradient;
+};
+export { Gradients, Gradient };
+//# sourceMappingURL=gradients.d.ts.map

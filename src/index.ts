c863dc17e66a9880d66cb9a74e1374821fbb7a31
avnerus <avnerus@gmail.com>
2024-04-09 17:37:45 +0000

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -26,12 +26,9 @@ import {
   Points,
   Camera,
   PerspectiveCamera,
-  WebGLRenderer,
   Texture,
   Euler,
   Quaternion,
-  WebGLRenderTarget,
-  Scene
 } from 'three';
 
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
